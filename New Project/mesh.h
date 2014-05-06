5ab1fc5a3fa9c02cea3e459627ba7c84d30ffbed
Entuser <entuser@yandex.ru>
2014-05-07 00:00:52 +0400

    Started working on mesh loading
    added objloader class (not mine) , started implementing mesh class

--- New Project/mesh.h
+++ New Project/mesh.h
@@ -1,10 +1,17 @@
 
 #pragma once
 
+#include "graph2D.h"
+
 class CMesh
 {
 private:
-
+	GLuint vBuff, uvBuff, iBuff, nBuff;
+	CGLTexture* bTex;
 public:
 
+	void setTexture(CGLTexture * tex);
+	bool loadFrom(const char* src);
+	CMesh();
+	~CMesh();
 };
