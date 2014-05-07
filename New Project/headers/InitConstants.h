ef0eb9b8e7987c5445aa2496db6e1db0d969a0a6
George <shaxirrim@gmail.com>
2014-05-07 04:34:38 +0400

    UPD

--- New Project/headers/InitConstants.h
+++ New Project/headers/InitConstants.h
@@ -1,7 +1,11 @@
-#ifndef INITIALCONSTANTS_H_INCLUDED
-#define INITIALCONSTANTS_H_INCLUDED
+#ifndef INITCONSTANTS_H_INCLUDED
+#define INITCONSTANTS_H_INCLUDED
 
 class Init_Constants {
+private:
+	// Assertion of constant values
+	bool Check();
+
 public:
     // Field Constants
     double Field_Width;
@@ -26,109 +30,14 @@ public:
 	//Graphics Constants
 	int resX, resY;
 
-private:
-    // Assertion of constant values
-    bool Check() {
-        if (this->Field_Width <= 0) {
-            std::cerr << "Field_Width isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Field_Length <= 0) {
-            std::cerr << "Field_Length isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Wall_Width < 0) {
-            std::cerr << "Wall_Width is negative!" << std::endl;
-            return false;
-        }
-        if (this->LightCycle_Length < 0) {
-            std::cerr << "LightCycle_Length is negative!" << std::endl;
-            return false;
-        }
-        if (this->LightCycle_Width < 0) {
-            std::cerr << "LightCycle_Width is negative!" << std::endl;
-            return false;
-        }
-        if (this->LightCycle_DefaultSpeed <= 0) {
-            std::cerr << "LightCycle_DefaultSpeed isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Rocket_Speed <= 0) {
-            std::cerr << "Rocket_Speed isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Rocket_Length <= 0) {
-            std::cerr << "Rocket_Length isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Bomb_Time <= 0) {
-            std::cerr << "Bomb_Time isn't positive!" << std::endl;
-            return false;
-        }
-        if (this->Bomb_Radius <= 0) {
-            std::cerr << "Bomb_Radius isn't positive!" << std::endl;
-            return false;
-        }
-        return true;
-    }
-
-public:
     // Initing constants
-    void Init_from_config_file() {
-        if (freopen("config.cfg", "r", stdin) == NULL) {
-            std::cerr << "No config.cfg file was found!" << std::endl;
-            throw(-1);
-        }
-
-        std::string s;
-
-        while (std::cin >> s) {
-            if (s == "Field_Width")
-                std::cin >> this->Field_Width;
-            else if (s == "Field_Length")
-                std::cin >> this->Field_Length;
-            else if (s == "Wall_Width")
-                std::cin >> this->Wall_Width;
-            else if (s == "LightCycle_Length")
-                std::cin >> this->LightCycle_Length;
-            else if (s == "LightCycle_Width")
-                std::cin >> this->LightCycle_Width;
-            else if (s == "LightCycle_DefaultSpeed")
-                std::cin >> this->LightCycle_DefaultSpeed;
-            else if (s == "Rocket_Speed")
-                std::cin >> this->Rocket_Speed;
-            else if (s == "Rocket_Length")
-                std::cin >> this->Rocket_Length;
-            else if (s == "Bomb_Time")
-                std::cin >> this->Bomb_Time;
-            else if (s == "Bomb_Radius")
-                std::cin >> this->Bomb_Radius;
-			else if(s == "ResolutionX")
-				std::cin >> resX;
-			else if(s == "ResolutionY")
-				std::cin >> resY;
-			else 
-                std::cerr << "Error in reading config.cfg file: unknown attribte:" << s << std::endl;
-
-            std::getline(std::cin, s);
-        }
-
-        fclose(stdin);
-        if (!Check()) {
-            std::cerr << "Constant check failed!" << std::endl;
-            throw -1;
-        }
-    }
+	void Init_from_config_file();
 
     // Constructor
-    Init_Constants() {
-        Init_from_config_file();
-    }
+	Init_Constants();
 
     // Destructor
-    ~Init_Constants() {
-
-    }
+	~Init_Constants();
 };
 
-#endif // INITIALCONSTANTS_H_INCLUDED
+#endif // INITCONSTANTS_H_INCLUDED
---
+++ New Project/sources/InitConstants.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "headers\InitConstants.h"
+
+bool Init_Constants::Check() {
+	if (this->Field_Width <= 0) {
+		std::cerr << "Field_Width isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Field_Length <= 0) {
+		std::cerr << "Field_Length isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Wall_Width < 0) {
+		std::cerr << "Wall_Width is negative!" << std::endl;
+		return false;
+	}
+	if (this->LightCycle_Length < 0) {
+		std::cerr << "LightCycle_Length is negative!" << std::endl;
+		return false;
+	}
+	if (this->LightCycle_Width < 0) {
+		std::cerr << "LightCycle_Width is negative!" << std::endl;
+		return false;
+	}
+	if (this->LightCycle_DefaultSpeed <= 0) {
+		std::cerr << "LightCycle_DefaultSpeed isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Rocket_Speed <= 0) {
+		std::cerr << "Rocket_Speed isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Rocket_Length <= 0) {
+		std::cerr << "Rocket_Length isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Bomb_Time <= 0) {
+		std::cerr << "Bomb_Time isn't positive!" << std::endl;
+		return false;
+	}
+	if (this->Bomb_Radius <= 0) {
+		std::cerr << "Bomb_Radius isn't positive!" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+void Init_Constants::Init_from_config_file() {
+	if (freopen("config.cfg", "r", stdin) == NULL) {
+		std::cerr << "No config.cfg file was found!" << std::endl;
+		throw(-1);
+	}
+
+	std::string s;
+
+	while (std::cin >> s) {
+		if (s == "Field_Width")
+			std::cin >> this->Field_Width;
+		else if (s == "Field_Length")
+			std::cin >> this->Field_Length;
+		else if (s == "Wall_Width")
+			std::cin >> this->Wall_Width;
+		else if (s == "LightCycle_Length")
+			std::cin >> this->LightCycle_Length;
+		else if (s == "LightCycle_Width")
+			std::cin >> this->LightCycle_Width;
+		else if (s == "LightCycle_DefaultSpeed")
+			std::cin >> this->LightCycle_DefaultSpeed;
+		else if (s == "Rocket_Speed")
+			std::cin >> this->Rocket_Speed;
+		else if (s == "Rocket_Length")
+			std::cin >> this->Rocket_Length;
+		else if (s == "Bomb_Time")
+			std::cin >> this->Bomb_Time;
+		else if (s == "Bomb_Radius")
+			std::cin >> this->Bomb_Radius;
+		else if (s == "ResolutionX")
+			std::cin >> resX;
+		else if (s == "ResolutionY")
+			std::cin >> resY;
+		else
+			std::cerr << "Error in reading config.cfg file: unknown attribte:" << s << std::endl;
+
+		std::getline(std::cin, s);
+	}
+
+	fclose(stdin);
+	if (!Check()) {
+		std::cerr << "Constant check failed!" << std::endl;
+		throw - 1;
+	}
+}
+
+Init_Constants::Init_Constants() {
+	Init_from_config_file();
+}
+
+Init_Constants::~Init_Constants() {
+
+}
