b9482fe29b2dfbdd6d317ea691c7fb0f4c8556cf
Aivoner <aivon94@mail.ru>
2014-05-09 18:55:48 +0400

    APD

--- New Project/headers/Game_Engine.h
+++ New Project/headers/Game_Engine.h
@@ -6,11 +6,11 @@
 class Game_Engine {
 private:
 	// Converts from game classes to polygon
+	Polygon2D < double > Polygon_from(const Point2D < double > &C_p, const Vector2D < double > &V, const double w);
 	Polygon2D < double > Polygon_from_cycle(const LightCycle &Cycle);
-	Polygon2D < double > Polygon_from_wall(const Wall &Segment);
+	Polygon2D < double > Polygon_from_wall(const Wall &_Wall);
 
 	// Intersects
-	bool Intersect_in_turning(const Polygon2D < double > &Turning, const Point2D < double > &TurnOf, const Polygon2D < double > &Static, const double &alpha);
 	bool Intersect(const LightCycle &Cycle, const Wall &_Wall);
 	bool Intersect(const LightCycle &Cycle1, const LightCycle &Cycle2);
 
--- New Project/sources/Game_Engine.cpp
+++ New Project/sources/Game_Engine.cpp
@@ -1,23 +1,31 @@
 #include "stdafx.h"
 #include "headers\Game_Engine.h"
 
-Polygon2D < double > Game_Engine::Polygon_from_cycle(const LightCycle &Cycle) {
-	return Polygon2D < double >();
-}
-
-Polygon2D < double > Game_Engine::Polygon_from_wall(const Wall &Segment) {
-	return Polygon2D < double >();
-}
-
-bool Game_Engine::Intersect_in_turning(const Polygon2D < double > &Turning, const Point2D < double > &TurnOf,const Polygon2D < double > &Static, const double &alpha) {
-	Polygon2D < double > Turned = Turning;
-	double d_alpha = alpha/(2*this->Constants->LightCycle_Length/this->Constants->LightCycle_Width);
-	for (double temp = 0; temp <= alpha; temp += d_alpha) {
-		if (Cross_polygon(Turned, Static))
-			return true;
-		Turned = turn(Turning, TurnOf, temp);
+Polygon2D < double > Game_Engine::Polygon_from(const Point2D < double > &C_p, const Vector2D < double > &V, const double w) {
+	vector < Point2D < double > > Pol;
+	if (V.y == 0) {
+		Pol.push_back(Point2D < double >(C_p.x - w / 2.0, C_p.y));
+		Pol.push_back(Point2D < double >(C_p.x + w / 2.0, C_p.y));
+		Pol.push_back(Pol[0] + V);
+		Pol.push_back(Pol[1] + V);
 	}
-	return false;
+	else {
+		Pol.push_back(Point2D < double >(C_p.x + w / (2.0*sqrt(1 + _SQR(V.x / V.y))), 0));
+		Pol[0].y = C_p.y - V.x*(Pol[0].x - C_p.x) / V.y;
+		Pol.push_back(Point2D < double >(C_p.x - w / (2.0*sqrt(1 + _SQR(V.x / V.y))), 0));
+		Pol[1].y = C_p.y - V.x*(Pol[1].x - C_p.x) / V.y;
+		Pol.push_back(Pol[1] + V);
+		Pol.push_back(Pol[0] + V);
+	}
+	return Polygon2D < double >(Pol);
+}
+
+Polygon2D < double > Game_Engine::Polygon_from_cycle(const LightCycle &Cycle) {
+	return Polygon_from(Cycle.Current_Point, this->Constants->LightCycle_Length * Norm(Cycle.Direction), this->Constants->LightCycle_Width);
+}
+
+Polygon2D < double > Game_Engine::Polygon_from_wall(const Wall &_Wall) {
+	return Polygon_from(_Wall.Segment.A, Vector2D < double >(_Wall.Segment.A, _Wall.Segment.B), this->Constants->Wall_Width);
 }
 
 bool Game_Engine::Intersect(const LightCycle &Cycle, const Wall &_Wall) {
