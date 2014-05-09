e523aa2fdcb6f14857ba29e462b764dca151b8a9
Aivoner <aivon94@mail.ru>
2014-05-09 18:15:50 +0400

    UPD

--- New Project/headers/Game_Engine.h
+++ New Project/headers/Game_Engine.h
@@ -10,7 +10,7 @@ private:
 	Polygon2D < double > Polygon_from_wall(const Wall &Segment);
 
 	// Intersects
-	bool Intersect_in_turning(const Polygon2D < double > &Turned, const Polygon2D < double > &Static, const double &alpha);
+	bool Intersect_in_turning(const Polygon2D < double > &Turning, const Point2D < double > &TurnOf, const Polygon2D < double > &Static, const double &alpha);
 	bool Intersect(const LightCycle &Cycle, const Wall &_Wall);
 	bool Intersect(const LightCycle &Cycle1, const LightCycle &Cycle2);
 
--- New Project/sources/Game_Engine.cpp
+++ New Project/sources/Game_Engine.cpp
@@ -9,13 +9,13 @@ Polygon2D < double > Game_Engine::Polygon_from_wall(const Wall &Segment) {
 	return Polygon2D < double >();
 }
 
-bool Game_Engine::Intersect_in_turning(const Polygon2D < double > &Turning, const Polygon2D < double > &Static, const double &alpha) {
+bool Game_Engine::Intersect_in_turning(const Polygon2D < double > &Turning, const Point2D < double > &TurnOf,const Polygon2D < double > &Static, const double &alpha) {
 	Polygon2D < double > Turned = Turning;
 	double d_alpha = alpha/(2*this->Constants->LightCycle_Length/this->Constants->LightCycle_Width);
 	for (double temp = 0; temp <= alpha; temp += d_alpha) {
 		if (Cross_polygon(Turned, Static))
 			return true;
-		Turned = turn(Turning, temp);
+		Turned = turn(Turning, TurnOf, temp);
 	}
 	return false;
 }
