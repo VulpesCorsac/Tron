2001161dd796cf94e6ebe959627385c5f53efb5c
George <shaxirrim@gmail.com>
2014-05-20 15:55:26 +0400

    0_О

--- New Project/headers/Wall.h
+++ New Project/headers/Wall.h
@@ -6,14 +6,20 @@
 class Wall {
 public:
     Segment2D < double > Segment;
-    int PlayerNumber;
+    int Player_Number;
+	int Wall_Number;
 
 	// Modify a wall
-	void Modify(const Segment2D < double > &_Segment, const int Player_Number);
+	void Modify(const Segment2D < double > &_Segment, const int &PlayerNumber, const int &WallNumber);
 	void Prolong(const Point2D < double > Point);
 
-	Wall(const Segment2D < double > &_Segment, const int Player_Number);
+	Wall(const Segment2D < double > &_Segment, const int &PlayerNumber, const int &WallNumber);
 	~Wall();
+
+	Wall &operator = (const Wall &_Wall);
+	friend bool operator == (const Wall &Wall1, const Wall &Wall2);
+	friend bool operator != (const Wall &Wall1, const Wall &Wall2);
+
 };
 
 #endif // WALL_H_INCLUDED
--- New Project/sources/Wall.cpp
+++ New Project/sources/Wall.cpp
@@ -1,22 +1,41 @@
 #include "stdafx.h"
 #include "headers\Wall.h"
 
-void Wall::Modify(const Segment2D < double > &_Segment, const int Player_Number) {
-	this->PlayerNumber = Player_Number;
+void Wall::Modify(const Segment2D < double > &_Segment, const int &PlayerNumber, const int &WallNumber) {
+	this->Player_Number = PlayerNumber;
+	this->Wall_Number = WallNumber;
 	this->Segment = _Segment;
 	return;
 }
 
 void Wall::Prolong(const Point2D < double > Point) {
 	Segment2D < double > Temp(this->Segment.A, Point);
-	Modify(Temp, this->PlayerNumber);
+	Modify(Temp, this->Player_Number, this->Wall_Number);
 	return;
 }
 
-Wall::Wall(const Segment2D < double > &_Segment, const int Player_Number) {
-	Modify(_Segment, this->PlayerNumber);
+Wall::Wall(const Segment2D < double > &_Segment, const int &PlayerNumber, const int &WallNumber) {
+	Modify(_Segment, PlayerNumber, WallNumber);
 }
 
 Wall::~Wall(void) {
 
+}
+
+Wall &Wall::operator = (const Wall &_Wall) {
+	if (this == &_Wall)
+		return *this;
+	this->Segment = _Wall.Segment;
+	this->Player_Number = _Wall.Player_Number;
+	return *this;
+}
+
+bool operator == (const Wall &Wall1, const Wall &Wall2) {
+	return (Wall1.Player_Number == Wall2.Player_Number &&
+		    Wall1.Segment == Wall2.Segment &&
+			Wall1.Wall_Number == Wall2.Wall_Number);
+}
+
+bool operator != (const Wall &Wall1, const Wall &Wall2) {
+	return (!(Wall1 == Wall2));
 }
